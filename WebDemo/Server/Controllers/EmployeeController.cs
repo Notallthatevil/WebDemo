0da7a60cbabaeb857a5febb964ee3255dc163f6b
Nate Tripp <nate.tripp13448@gmail.com>
2023-05-25 16:48:48 -0600

    Added ability to delete employee

--- WebDemo/Server/Controllers/EmployeeController.cs
+++ WebDemo/Server/Controllers/EmployeeController.cs
@@ -25,7 +25,7 @@ namespace WebDemo.Server.Controllers
 
 		[Route("[action]")]
 		[HttpPost]
-		public ActionResult AddEmployee([FromBody] Shared.Employee employee)
+		public ActionResult AddEmployee(Shared.Employee employee)
 		{
 			try
 			{
@@ -54,7 +54,65 @@ namespace WebDemo.Server.Controllers
 
 		[Route("[action]")]
 		[HttpPost]
-		public ActionResult DeleteEmployee([FromBody] Shared.Employee employee)
+		public ActionResult UpdateEmployee(Shared.Employee employee)
+		{
+			try
+			{
+				using (var transaction = _dbContext.Database.BeginTransaction())
+				{
+					var dep = _dbContext.Departments.SingleOrDefault(d => d.Name.ToLower() == employee.Department.ToLower());
+					if (dep == null)
+					{
+						dep = new Models.Department { Name = employee.Department };
+						var result = _dbContext.Departments.Add(dep);
+						_dbContext.SaveChanges();
+					}
+
+					var emp = _dbContext.Employees.SingleOrDefault(e => e.Id == employee.Id);
+					if (emp != null)
+					{
+						emp.FirstName = employee.FirstName;
+						emp.LastName = employee.LastName;
+						emp.Title = employee.Title;
+						emp.DepartmentId = dep.Id;
+
+						_dbContext.Employees.Update(emp);
+						_dbContext.SaveChanges();
+						transaction.Commit();
+					}
+					else
+					{
+						transaction.Rollback();
+						return BadRequest("Failed to retrieve the employee!");
+					}
+				}
+
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[Route("[action]")]
+		[HttpPost]
+		public ActionResult GetById([FromBody] int id)
+		{
+			var emp = _dbContext.Employees.SingleOrDefault(e => e.Id == id);
+			if (emp != null)
+			{
+				return Json(new Shared.Employee { Id = emp.Id, FirstName = emp.FirstName, LastName = emp.LastName, Department = _dbContext.Departments.SingleOrDefault(d => d.Id == emp.DepartmentId)?.Name ?? "<Not Assigned>", Title = emp.Title });
+			}
+			else
+			{
+				return Json(null);
+			}
+		}
+
+		[Route("[action]")]
+		[HttpPost]
+		public ActionResult DeleteEmployee( Shared.Employee employee)
 		{
 			try
 			{
